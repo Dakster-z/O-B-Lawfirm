6c92db329e932d30997c775d7a6b58143fa7975d
zaid dakir <zaid.dakengineer@gmail.com>
2024-02-13 13:56:38 +0100

    last update

---
+++ js/main.js
@@ -0,0 +1,117 @@
+﻿(function ($) {
+    "use strict";
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "73px");
+        } else {
+            $('.nav-bar').removeClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "0");
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+    
+    // Blogs carousel
+    $(".blog-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+    
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+})(jQuery);
+
+let btnEnvoyer = document.querySelector("button");
+console.log(btnEnvoyer);
+
+btnEnvoyer.addEventListener("click", () => {
+    localStorage.setItem("Nom et prénom", document.queryselector("#name").value);
+    localStorage.setItem("tel", document.queryselector("#phone").value);
+    localStorage.setItem("Adresse e-mail", document.queryselector("#email").value);
+    localStorage.setItem("Objet du rendez vous", document.queryselector("#message").value);
+    console.log(document.querySelector("#name"));
+
+});
---
+++ submit.php
@@ -0,0 +1,18 @@
+﻿<?php
+if (isset($_POST["name"]) && isset($_POST["phone"]) && isset($_POST["email"]) && isset($_POST["message"])) {
+  $name = $_POST["name"];
+  $phone = $_POST["phone"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  $file = fopen("submissions.txt", "a");
+  fwrite($file, "Nom et prénom : $name\n");
+  fwrite($file, "Numéro de téléphone : $phone\n");
+  fwrite($file, "Adresse e-mail : $email\n");
+  fwrite($file, "Objet du rendez-vous : $message\n\n");
+  fclose($file);
+
+  header("Location: about.html");
+  exit;
+}
+?>
